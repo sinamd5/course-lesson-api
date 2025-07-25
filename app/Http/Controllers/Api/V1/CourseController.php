94c05f85a8ec0b464f6ae52ae4e0d8864569fa93
sina <sina.cplusplus@gmail.com>
2025-07-26 02:58:12 +0330

    storeLesson functionality has been done by StoreLessonRequest FormRequest

--- app/Http/Controllers/Api/V1/CourseController.php
+++ app/Http/Controllers/Api/V1/CourseController.php
@@ -4,16 +4,24 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreLessonRequest;
+use App\Models\Course;
+
 
 class CourseController extends Controller
 {
-    public function storeLesson(Request $request, $courseId){
-        // Later: Validate, find course, create lesson
-        return response()->json([
-            'message' => 'storeLesson reached!',
-            'courseId' => $courseId,
-            'data' => $request->all()
+    public function storeLesson(StoreLessonRequest $request, $courseId){
+        $course = Course::findOrFail($courseId);
+
+        $lesson = $course->lessons()->create([
+            'name' => $request->name,
+            'price' => $request->price,
         ]);
+
+        return response()->json([
+            'message' => 'Lesson created successfully',
+            'data' => $lesson,
+        ], 201);
     }
 
     public function updateCoursePrice(Request $request, $courseId){
